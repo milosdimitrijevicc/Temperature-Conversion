66a4fdf77ae5aef32b3896abd2558f68b3fe91e5
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-04-28 19:00:57 +0200

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('test');
+const btn = document.querySelector('button');
+const input = document.querySelector('#textBox');
+const toFahreheit = document.querySelector('#toFahreheit');
+const toCelsius = document.querySelector('#toCelsius');
+const result = document.querySelector('#result');
+let temp;
+
+btn.addEventListener('click' , function() {
+
+  if(toFahreheit.checked){
+    temp = Number(input.value);
+    temp = temp * 9 / 5 + 32;
+    result.textContent = `${temp.toFixed(1)}°F`;
+  }
+  else if(toCelsius.checked){
+    temp = Number(input.value);
+    temp = (temp - 32) * (5/9);
+    result.textContent = `${temp.toFixed(1)}°C`;
+  }
+  else{
+    result.textContent = "Select a unit!"
+  }
+
+});
+
